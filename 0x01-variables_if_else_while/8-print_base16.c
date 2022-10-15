aa48698867114f146122d338096c03b2bb3e2a15
imiebaka <triple07even@gmail.com>
2022-10-15 14:56:55 -0700

    task 8 fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
 /**
- *main - print alphabet reversed using putchar
- *Return: Always 0 (Success)
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-char alpha = 'z';
+	int n;
+	char ch;
 
-while (alpha >= 'a')
-{
-putchar(alpha);
-alpha--;
-}
-putchar('\n');
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
 
-return (0);
+	return (0);
 }
