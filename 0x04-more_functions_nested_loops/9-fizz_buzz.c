a5003c298710413e835115f4478d44332df20ad8
imiebaka <triple07even@gmail.com>
2022-10-21 03:32:06 -0700

    adding task 10

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry piont
+ * Return: Always 0
+ */
+
+int main(void)
+{
+        int start = 1;
+        int end = 100;
+        for (start = 0; start < end; start++)
+        {
+                if (start % 3 == 0 && start % 5 == 0)
+                {
+                        printf("FizzBuzz");
+                }
+                else if (start % 5 == 0)
+                {
+                        printf("Buzz");
+                }
+                else if (start % 3 == 0)
+                {
+                        printf("Fizz");
+                }
+                printf("\n");
+        }
+
+        return 0;
+}
